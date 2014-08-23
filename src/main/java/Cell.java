26a5aae9a6ee0c3902ab7f1c1190c20399760576
scoobycoder <scoobycoder@gmail.com>
2014-08-23 14:50:23 -0400

    cell and world created with test

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,6 +1,9 @@
 
 public class Cell {
 
+	public Cell(int x, int y) {
+	}
+
 	public boolean alive() {
 		return true;
 	}
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,17 +1,32 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 
 public class GameTest {
 
 	@Test
-	public void cellShouldReportLifeIfAlive() {
+	public void cellShouldTakeBirthLocation() {
+		Cell cell =  new Cell(0,0);
 		
-		
-		Cell cell = new Cell();
 		assertEquals(true, cell.alive());
 	}
+	
+	@Test
+	public void worldShouldReturnTwoCellsIfTwoCellsLive() {
+		List<Cell> cellList = new ArrayList<Cell>();
+		Cell cell = new Cell(0,0);
+		Cell cell2 = new Cell(1,0);
+		cellList.add(cell);
+		cellList.add(cell2);
 
+		World world = new World(cellList);
+		
+		assertEquals(2, world.cellCount());
+		
+	}
 	
 }

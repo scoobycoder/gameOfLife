60adc96c0d0c263db07f9d87586a214224775457
scoobycoder <scoobycoder@gmail.com>
2014-11-30 17:33:00 -0500

    refactor test for world creation in before

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,32 +1,37 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GameTest {
 	
+	private World world;
+
+	@Before
+	public void setup() {
+		world = new World();
+	}
+	
 	@Test
-	public void testNewWorldIsEmpty() {
-		World world = new World();
+	public void newWorldIsEmpty() {
 		assertEquals(true, world.isEmpty());
 	}
 	
 	@Test
-	public void testWorldStillEmptyAfterTick() {
-		World world = new World();
+	public void worldStillEmptyAfterTick() {
 		world.tick();
 		
 		assertEquals(true, world.isEmpty());
 	}
 	
 	@Test
-	public void testAddingCellLocationBeforeTick() {
-		World world = new World();
-		
+	public void addingCellLocationBeforeTick() {
 		Location location = new Location();
 		world.addCellLocation(location);
 		
 		assertEquals(false, world.isEmpty());
 	}
+	
 	
 }
